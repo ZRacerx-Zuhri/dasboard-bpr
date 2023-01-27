9e4addf1a3343a54dea4ebe1334524e4fc2b607c
Putra Nugroho <50862961+putranugroho@users.noreply.github.com>
2023-01-27 09:16:18 +0700

    Add Dummy Data

--- src/views/history/history/DetailHistory.js
+++ src/views/history/history/DetailHistory.js
@@ -1,4 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React, {
+  // useState,
+  useEffect,
+} from 'react'
 // import { useLocation } from 'react-router-dom'
 // import { useFetch } from '../../../action'
 
@@ -18,10 +21,83 @@ import {
 } from '@coreui/react'
 
 const DetailPpob = () => {
-  const [trans, setTrans] = useState([])
-  console.log(setTrans)
+  // const [trans, setTrans] = useState([])
   // const url = `https://gw-dev-api.medtransdigital.com/dashboard/get_trans?bpr_id=2640&tcode=${location.state.userChoice}`
   // useFetch({ url, onSuccess: (data) => setTrans(data.data) })
+
+  const data = [
+    {
+      date: '08:56:50 | January 27th 2023',
+      jenis_transaksi: 'Transfer',
+      keterangan_transaksi: 'Transfer ke BPR Indra',
+      data_transaksi: 'John Doe 1234567890',
+      status: '1',
+      debit: 50000,
+      kredit: 0,
+      rrn: '000035',
+    },
+    {
+      date: '08:56:50 | January 27th 2023',
+      jenis_transaksi: 'Transfer',
+      keterangan_transaksi: 'Transfer dari BPR Tegal',
+      data_transaksi: 'John Pantau 1001002003004',
+      status: '1',
+      debit: 0,
+      kredit: 100000,
+      rrn: '000028',
+    },
+    {
+      date: '08:56:50 | January 27th 2023',
+      jenis_transaksi: 'Bayar/Top-Up',
+      keterangan_transaksi: 'Pembayaran Telkomsel Prepaid',
+      data_transaksi: '081234567890',
+      status: '1',
+      debit: 72500,
+      kredit: 0,
+      rrn: '000002',
+    },
+    {
+      date: '08:56:50 | January 27th 2023',
+      jenis_transaksi: 'Biaya',
+      keterangan_transaksi: 'Pembayaran Telkomsel Prepaid',
+      data_transaksi: '081234567890',
+      status: '1',
+      debit: 1500,
+      kredit: 0,
+      rrn: '000002',
+    },
+    {
+      date: '08:56:50 | January 26th 2023',
+      jenis_transaksi: 'Transfer',
+      keterangan_transaksi: 'Transfeer ke BPR Indra',
+      data_transaksi: 'John Doe 1234567890',
+      status: '1',
+      debit: 50000,
+      kredit: 0,
+      rrn: '000001',
+    },
+    {
+      date: '08:56:50 | January 27th 2023',
+      jenis_transaksi: 'Tarik Tunai',
+      keterangan_transaksi: 'Penarikan tunai di ATM Pusat',
+      data_transaksi: 'S1GPMK10RA',
+      status: '1',
+      debit: 300000,
+      kredit: 0,
+      rrn: '000013',
+    },
+    {
+      date: '08:56:50 | January 27th 2023',
+      jenis_transaksi: 'Transfer',
+      keterangan_transaksi: 'Transfeer dari Bank Mandiri',
+      data_transaksi: 'John Doe 12700012312345',
+      status: '0',
+      debit: 0,
+      kredit: 150000,
+      rrn: '000001',
+    },
+  ]
+
   useEffect(() => {
     // fetch(
     //   `https://gw-dev-api.medtransdigital.com/dashboard/get_trans?bpr_id=${location.state.bprChoice}&tcode=${location.state.transChoice}`,
@@ -47,34 +123,38 @@ const DetailPpob = () => {
                     <CTableHeaderCell>Jenis Transaksi</CTableHeaderCell>
                     <CTableHeaderCell>Keterangan Transaksi</CTableHeaderCell>
                     <CTableHeaderCell>Data Transaksi</CTableHeaderCell>
+                    <CTableHeaderCell>Status</CTableHeaderCell>
                     <CTableHeaderCell>Debit</CTableHeaderCell>
                     <CTableHeaderCell>Kredit</CTableHeaderCell>
                     <CTableHeaderCell>RRN</CTableHeaderCell>
                   </CTableRow>
                 </CTableHead>
                 <CTableBody>
-                  {trans.map((item, index) => (
+                  {data.map((item, index) => (
                     <CTableRow v-for="item in tableItems" key={index}>
                       <CTableDataCell>
-                        <div>{item.tgl_trans}</div>
+                        <div>{item.date}</div>
                       </CTableDataCell>
                       <CTableDataCell>
-                        <div>{item.no_hp}</div>
+                        <div>{item.jenis_transaksi}</div>
+                      </CTableDataCell>
+                      <CTableDataCell>
+                        <div>{item.keterangan_transaksi}</div>
+                      </CTableDataCell>
+                      <CTableDataCell>
+                        <div>{item.data_transaksi}</div>
+                      </CTableDataCell>
+                      <CTableDataCell>
+                        {item.status === '1' ? <div>Approve</div> : <div>Rejected</div>}
+                      </CTableDataCell>
+                      <CTableDataCell>
+                        <div>Rp. {item.debit}</div>
+                      </CTableDataCell>
+                      <CTableDataCell>
+                        <div>Rp. {item.kredit}</div>
                       </CTableDataCell>
                       <CTableDataCell>
                         <div>{item.rrn}</div>
-                      </CTableDataCell>
-                      <CTableDataCell>
-                        {item.status_rek === '1' ? <div>Approve</div> : <div>Rejected</div>}
-                      </CTableDataCell>
-                      <CTableDataCell>
-                        <div>{item.ket_trans}</div>
-                      </CTableDataCell>
-                      <CTableDataCell>
-                        <div>Rp. {item.amount}</div>
-                      </CTableDataCell>
-                      <CTableDataCell>
-                        <div>Rp. {item.admin_fee}</div>
                       </CTableDataCell>
                     </CTableRow>
                   ))}
